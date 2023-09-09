4d97672cdbb1a5447444d002e5b01bbd270317ba
Megan <esler6724@gmail.com>
2023-09-09 15:00:18 -0700

    commit tree 336b2baa52d86fae4616630dbb812cc58372dfb0

--- config/environment.rb
+++ config/environment.rb
@@ -10,3 +10,11 @@ ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "./db/test.sqlite3"
 )
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  )
+SQL
+
